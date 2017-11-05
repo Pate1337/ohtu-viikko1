a2a93368f3778bf0ebe59ca6fa84cd41bfb6d3d2
Pate1337 <paavo.hemmo@hotmail.com>
2017-11-05 23:57:09 +0200

    Tehtävä 16 jäi pahasti kesken

---
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author paavo
+ */
+public class StatisticsTest {
+    
+    Reader readerStub = new Reader() {
+ 
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
+ 
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+ 
+            return players;
+        }
+    };
+ 
+    Statistics stats;
+    
+//    public StatisticsTest() {
+//    }
+//    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+    
+    @Before
+    public void setUp() {
+        stats = new Statistics(readerStub);
+    }
+    
+    @Test
+    public void palauttaaJoukkueenPelaajat() {
+        
+    }
+    
+//    @After
+//    public void tearDown() {
+//    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
